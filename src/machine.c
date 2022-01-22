720ea4d4c90e5e9f47d087eb78d3e6058e753d0d
Yanjie He <heyanjie0@outlook.com>
2022-01-22 18:24:48 -0500

    arithmetic operations

--- src/machine.c
+++ src/machine.c
@@ -1,8 +1,8 @@
 #include "machine.h"
 #include "opcode.h"
 #include "type.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #define STACK_PUSH_I32(VALUE)                                                  \
   sp++;                                                                        \
@@ -60,6 +60,7 @@ Machine *create_machine(i32 stack_max_size) {
 }
 
 void run_machine(Machine *machine) {
+  /* state */
   Byte *code;
   i32 code_length;
   Byte op;
@@ -67,6 +68,10 @@ void run_machine(Machine *machine) {
   i32 sp;
   i32 fp;
   i32 pc;
+
+  /* type comparison for binary operations */
+  u32 left_type;
+  u32 right_type;
 
   code = machine->env.function->code;
   code_length = machine->env.function->code_length;
@@ -97,7 +102,129 @@ void run_machine(Machine *machine) {
       sp--;
       break;
     }
+    case SUB_I32: {
+      stack[sp - 1].u.i32_v = (stack[sp - 1].u.i32_v - stack[sp].u.i32_v);
+      stack[sp].type = TYPE_EMPTY;
+      sp--;
+      break;
     }
-
+    case MUL_I32: {
+      stack[sp - 1].u.i32_v = (stack[sp - 1].u.i32_v * stack[sp].u.i32_v);
+      stack[sp].type = TYPE_EMPTY;
+      sp--;
+      break;
+    }
+    case DIV_I32: {
+      stack[sp - 1].u.i32_v = (stack[sp - 1].u.i32_v / stack[sp].u.i32_v);
+      stack[sp].type = TYPE_EMPTY;
+      sp--;
+      break;
+    }
+    case ADD_I64: {
+      stack[sp - 1].u.i64_v = (stack[sp - 1].u.i64_v + stack[sp].u.i64_v);
+      stack[sp].type = TYPE_EMPTY;
+      sp--;
+      break;
+    }
+    case SUB_I64: {
+      stack[sp - 1].u.i64_v = (stack[sp - 1].u.i64_v - stack[sp].u.i64_v);
+      stack[sp].type = TYPE_EMPTY;
+      sp--;
+      break;
+    }
+    case MUL_I64: {
+      stack[sp - 1].u.i64_v = (stack[sp - 1].u.i64_v * stack[sp].u.i64_v);
+      stack[sp].type = TYPE_EMPTY;
+      sp--;
+      break;
+    }
+    case DIV_I64: {
+      stack[sp - 1].u.i64_v = (stack[sp - 1].u.i64_v / stack[sp].u.i64_v);
+      stack[sp].type = TYPE_EMPTY;
+      sp--;
+      break;
+    }
+    case ADD_F32: {
+      stack[sp - 1].u.f32_v = (stack[sp - 1].u.f32_v + stack[sp].u.f32_v);
+      stack[sp].type = TYPE_EMPTY;
+      sp--;
+      break;
+    }
+    case SUB_F32: {
+      stack[sp - 1].u.f32_v = (stack[sp - 1].u.f32_v - stack[sp].u.f32_v);
+      stack[sp].type = TYPE_EMPTY;
+      sp--;
+      break;
+    }
+    case MUL_F32: {
+      stack[sp - 1].u.f32_v = (stack[sp - 1].u.f32_v * stack[sp].u.f32_v);
+      stack[sp].type = TYPE_EMPTY;
+      sp--;
+      break;
+    }
+    case DIV_F32: {
+      stack[sp - 1].u.f32_v = (stack[sp - 1].u.f32_v / stack[sp].u.f32_v);
+      stack[sp].type = TYPE_EMPTY;
+      sp--;
+      break;
+    }
+    case ADD_F64: {
+      stack[sp - 1].u.f64_v = (stack[sp - 1].u.f64_v + stack[sp].u.f64_v);
+      stack[sp].type = TYPE_EMPTY;
+      sp--;
+      break;
+    }
+    case SUB_F64: {
+      stack[sp - 1].u.f64_v = (stack[sp - 1].u.f64_v - stack[sp].u.f64_v);
+      stack[sp].type = TYPE_EMPTY;
+      sp--;
+      break;
+    }
+    case MUL_F64: {
+      stack[sp - 1].u.f64_v = (stack[sp - 1].u.f64_v * stack[sp].u.f64_v);
+      stack[sp].type = TYPE_EMPTY;
+      sp--;
+      break;
+    }
+    case DIV_F64: {
+      stack[sp - 1].u.f64_v = (stack[sp - 1].u.f64_v / stack[sp].u.f64_v);
+      stack[sp].type = TYPE_EMPTY;
+      sp--;
+      break;
+    }
+    case ADD_DYNAMIC: {
+      left_type = stack[sp - 1].type;
+      right_type = stack[sp].type;
+      if (left_type == right_type) {
+        switch (left_type) {
+        case TYPE_I32: {
+          stack[sp - 1].u.i32_v = (stack[sp - 1].u.i32_v + stack[sp].u.i32_v);
+          break;
+        }
+        case TYPE_I64: {
+          stack[sp - 1].u.i64_v = (stack[sp - 1].u.i64_v + stack[sp].u.i64_v);
+          break;
+        }
+        case TYPE_F32: {
+          stack[sp - 1].u.f32_v = (stack[sp - 1].u.f32_v + stack[sp].u.f32_v);
+          break;
+        }
+        case TYPE_F64: {
+          stack[sp - 1].u.f64_v = (stack[sp - 1].u.f64_v + stack[sp].u.f64_v);
+          break;
+        }
+        default: {
+          /* throw error */
+          break;
+        }
+          stack[sp].type = TYPE_EMPTY;
+          sp--;
+        }
+      } else {
+        /* throw error */
+      }
+      break;
+    }
+    }
   }
 }
