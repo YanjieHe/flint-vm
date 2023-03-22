5bedfb79cf588f4e5d535ff33f0e0c7568c97400
Yanjie He <heyanjie0@outlook.com>
2023-03-22 15:31:19 -0400

    update project structure

--- test/test.c
+++ test/test.c
@@ -2,6 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "test_arithmetic.h"
+#include "test_array.h"
+#include "test_function.h"
+#include "test_jump.h"
+#include "test_structure.h"
+#include "test_global_variable.h"
+#include "test_closure.h"
+
 int total_tests;
 int passed_tests;
 
@@ -34,4 +42,37 @@ void copy_byte_code(Function *function, Byte *code, size_t code_length) {
   function->code = malloc(sizeof(Byte) * code_length);
   memcpy(function->code, code, code_length);
   function->code_length = code_length;
+}
+
+int main(int argc, char **argv) {
+  total_tests = 0;
+  passed_tests = 0;
+
+  test_add();
+  test_subtract();
+
+  test_create_an_array();
+  test_create_an_illegal_array();
+  test_access_array();
+
+  test_function_invocation();
+  test_function_return_i32();
+  test_function_factorial();
+  test_function_native_function_call();
+  test_function_tail_call();
+  test_function_mutual_tail_recursion();
+
+  test_jump();
+  test_loop();
+
+  test_structure();
+  test_structure_get_field_value();
+
+  test_global_variable();
+
+  test_closure_invocation();
+
+  test_results_summary();
+
+  return 0;
 }
