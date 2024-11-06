b6e9a7d8cfb0e55e1802fa02bf1e36eb76b00da9
Yanjie He <heyanjie0@outlook.com>
2024-11-06 11:02:01 -0800

    Include the bytecode for the subtraction program in the code.

--- test/test_arithmetic.c
+++ test/test_arithmetic.c
@@ -1,15 +1,16 @@
 #include "test_arithmetic.h"
-#include "value.h"
-#include "opcode.h"
-#include "machine.h"
-#include <stdio.h>
-#include "test.h"
 #include "byte_code_loader.h"
+#include "machine.h"
+#include "opcode.h"
+#include "test.h"
+#include "value.h"
+#include <stdio.h>
 
 void test_add() {
   Program *program;
   Machine *machine;
-  Byte code[] = {PUSH_I32_1BYTE, 10, PUSH_I32_2BYTES, 2, 5, ADD_I32, PUSH_I32_0, HALT};
+  Byte code[] = {PUSH_I32_1BYTE, 10,  PUSH_I32_2BYTES, 2, 5, ADD_I32,
+                 PUSH_I32_0,     HALT};
 
   program = create_program_with_single_function(__FUNCTION__, code,
                                                 sizeof(code) / sizeof(Byte));
@@ -29,6 +30,15 @@ void test_subtract() {
   Program *program;
   Machine *machine;
   ByteCodeLoader *loader;
+  Byte code[] = {PUSH_I32_2BYTES,
+                 41,
+                 23, /* PUSH 10519 */
+                 PUSH_I32_1BYTE,
+                 55, /* PUSH 55 */
+                 SUB_I32,
+                 PUSH_I32_0, /* PUSH 0 */
+                 HALT};
+  size_t i;
 
   loader = create_byte_code_loader("byte_code/subtract");
   ASSERT_NOT_EQUAL(loader, NULL);
@@ -37,6 +47,11 @@ void test_subtract() {
   show_errors(loader->error_messages);
   ASSERT_NOT_EQUAL(program, NULL);
   ASSERT_EQUAL(loader->error_messages, NULL);
+
+  ASSERT_EQUAL(program->entry->code_length, sizeof(code) / sizeof(Byte));
+  for (i = 0; i < sizeof(code) / sizeof(Byte); i++) {
+    ASSERT_EQUAL(program->entry->code[i], code[i]);
+  }
 
   machine = create_machine(100);
   load_program(machine, program);
